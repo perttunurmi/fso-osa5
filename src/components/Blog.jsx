e94bfc5366bf94b362c93287bec59df25e883060
Perttu Nurmi <perttu.nurmi@gmail.com>
2025-08-17 11:15:59 +0300

    5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,49 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [showExtra, setShowExtra] = useState(false)
+
+  const toggleExtra = () => {
+    if (showExtra) {
+      setShowExtra(false)
+    } else {
+      setShowExtra(true)
+    }
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if (!showExtra) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author} <button onClick={toggleExtra}> show </button>
+      </div >
+    )
+  } else {
+    return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title} {blog.author} <button onClick={toggleExtra}> hide </button>
+        </div>
+        <div>
+          url: {blog.url || ""}
+        </div>
+        <div>
+          likes: {blog.likes || 0} <button> likes </button>
+        </div>
+        <div>
+          {blog.user.name || ""}
+        </div>
+      </div >
+    )
+  }
+
+}
 
 export default Blog
